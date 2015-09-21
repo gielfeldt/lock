8cde10bcdedeec6534201610c757dd1d6c853fcd
Thomas Gielfeldt <thomas@gielfeldt.dk>
2015-09-21 19:19:25 +0200

    Add file locking specific tests.

--- tests/LockFileTest.php
+++ tests/LockFileTest.php
@@ -17,8 +17,35 @@ include_once __DIR__ . '/LockTestBase.php';
  */
 class LockFileTest extends LockTestBase
 {
+    protected $path;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->path = sys_get_temp_dir() . '/' . uniqid();
+        mkdir($this->path);
+    }
+
     protected function getStorage()
     {
-        return new Lock\Storage\File();
+        return new Lock\Storage\File($this->path);
+    }
+
+    public function testMissingLock()
+    {
+        $lock1 = $this->service1->acquire('lock1');
+        $this->assertTrue($lock1 instanceof Lock\LockItemInterface);
+        unlink($this->path . '/lock.' . $lock1->getIdentifier());
+        $lock2 = $this->service1->loadCurrent('lock1');
+        $this->assertFalse($lock2);
+    }
+
+    public function testFileCreation()
+    {
+        $service = new Lock\LockService([
+            'storage' => new Lock\Storage\File($this->path . '/does/not/exist'),
+        ]);
+        $lock1 = $service->acquire('lock1');
+        $this->assertFalse($lock1 instanceof Lock\LockItemInterface);
     }
 }
